c07bd67ff0d0c26e3cd08551ecc40ebe53171d92
Lau Taarnskov <lau@taarnskov.com>
2013-04-22 18:19:04 -0300

    move AsciiRenderer to separate file

--- lib/letris/ascii_game.rb
+++ lib/letris/ascii_game.rb
@@ -1,8 +1,6 @@
-# coding: UTF-8
 require 'io/wait'
 SLEEP_FOR_LOOP = 0.02
-PIECE_COLORS = { :i => 36, :j => 34, :l => 93, :o => 33, :s => 32, :t => 35, :z => 31 }
-LEVELS = { 0 => 0.8,
+LEVELS = {  0 => 0.8,
             1 => 0.7167,
             2 => 0.6333,
             3 => 0.55,
@@ -13,35 +11,8 @@ LEVELS = { 0 => 0.8,
             8 => 0.1333,
             9 => 0.1 
    }
+
 module Letris
-  class AsciiRenderer
-    def colorized(piece_name)
-      piece_name == ' ' ? text = ' ' : text  = "X"
-      "\e[1;#{PIECE_COLORS[piece_name.downcase.to_sym]};40m"+text+"\e[0m"
-    end
-
-    def render_board(board, lines_cleared)
-      buf = '' 
-      buf = buf + "Keys: H: move left. L: move right. R: rotate piece. Q: quit\n"
-      buf = buf + "F: drop piece\n"
-      buf = buf + "Lines cleared: #{lines_cleared}\n"
-      y_pos = board.height - 1 
-      board.height.times do |y|
-        buf = buf + '' 
-        buf = buf + "║"
-        board.width.times 
-        board.width.times do |x| 
-          buf = buf + "#{colorized board.tile_for_xy(x,y_pos)}" 
-        end
-        buf = buf + "║"
-        buf = buf + "\n"
-        y_pos = y_pos - 1
-      end
-      buf = buf + "╚══════════╝"
-      buf
-    end
-  end
-
   class AsciiGame
     def render_board(board)
       @renderer ||= Letris::AsciiRenderer.new
---
+++ lib/letris/ascii_renderer.rb
@@ -0,0 +1,31 @@
+# coding: UTF-8
+PIECE_COLORS = { :i => 36, :j => 34, :l => 93, :o => 33, :s => 32, :t => 35, :z => 31 }
+module Letris
+  class AsciiRenderer
+    def colorized(piece_name)
+      piece_name == ' ' ? text = ' ' : text  = "X"
+      "\e[1;#{PIECE_COLORS[piece_name.downcase.to_sym]};40m"+text+"\e[0m"
+    end
+
+    def render_board(board, lines_cleared)
+      buf = '' 
+      buf = buf + "Keys: H: move left. L: move right. R: rotate piece. Q: quit\n"
+      buf = buf + "F: drop piece\n"
+      buf = buf + "Lines cleared: #{lines_cleared}\n"
+      y_pos = board.height - 1 
+      board.height.times do |y|
+        buf = buf + '' 
+        buf = buf + "║"
+        board.width.times 
+        board.width.times do |x| 
+          buf = buf + "#{colorized board.tile_for_xy(x,y_pos)}" 
+        end
+        buf = buf + "║"
+        buf = buf + "\n"
+        y_pos = y_pos - 1
+      end
+      buf = buf + "╚══════════╝"
+      buf
+    end
+  end
+end
